40dd89d89e5588ce58ceb4ea638f60d3fb4a1601
Rafael Corrêa Bitencourt <rafael2004.correa@gmail.com>
2023-03-17 23:38:45 -0300

    adicionando lógica de chutes e erros

--- forca.py
+++ forca.py
@@ -1,1 +1,22 @@
-
+while erro < 6:
+	desenho(erro)
+	for i in range (0, tamanho):
+        	print(espaço[i], end=" ")
+        print("")
+    	print("")
+	print("não tem: " + " / ".join(str(num) for num in descarte))
+	print("")
+	print("insira uma letra:")
+	chute = input()
+	if chute in palavra:
+		for i in range (0, tamanho):
+			if chute == palavra[i]:
+				espaço[i] = chute
+	elif chute in descarte:
+		pass
+	else:
+		erro += 1
+		descarte.append(chute)
+	os.system("clear")
+	if espaço == palavra:
+		break
